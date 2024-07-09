f66269cf3693ffd1b7bc086ae08f31d22520d802
aungmyatthu <aungmyatthu1998@gmail.com>
2024-07-09 15:33:41 +0100

    added button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
+import ReactAce from "react-ace";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+import {Button} from "@chakra-ui/react";
 
 function App() {
+
+ const [text, setText] = useState("");
+ function onChange(e){
+   setText(e)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <ReactAce
+          mode="python"
+          theme="github"
+          onChange={onChange}
+          name="UNIQUE_ID"
+          editorProps={{ $blockScrolling: true }}
+
+          />
+      </div>
+      <Button
+          onClick={() =>{
+              console.log(text)
+          }}>
+          Run
+      </Button>
     </div>
   );
 }
